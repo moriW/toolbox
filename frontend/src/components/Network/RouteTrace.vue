f27552c77c17a469236c17f41e6b93771b166200
Mori <yuhua.zhao@dragonplus.com>
2022-02-25 18:32:12 +0800

    add

--- frontend/src/components/Network/RouteTrace.vue
+++ frontend/src/components/Network/RouteTrace.vue
@@ -1,6 +1,22 @@
 <template>
-    <h3>RouteTrace</h3>
+  <h3>RouteTrace</h3>
+  <el-button @click=traceBack></el-button>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Traceroute } from 'nodejs-traceroute'
+
+export default defineComponent({
+    // setup() {},
+    methods: {
+        traceBack() {
+            Traceroute()
+            .on('hop', (hop) => {
+                console.log(hop)
+            })
+            .trace("aws.moridisa.com")
+        }
+    }
+})
 </script>
